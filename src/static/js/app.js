cfd4bf18f5b2d5a82f90d0e9dea758411c34996f
Roxana-Pohodnicaru <123390822+Roxana-Pohodnicaru@users.noreply.github.com>
2023-10-25 17:12:37 +0100

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -132,36 +132,11 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
         <Container fluid className={`item ${item.completed && 'completed'}`}>
             <Row>
                 <Col xs={1} className="text-center">
-                    <Button
-                        className="toggles"
-                        size="sm"
-                        variant="link"
-                        onClick={toggleCompletion}
-                        aria-label={
-                            item.completed
-                                ? 'Mark item as incomplete'
-                                : 'Mark item as complete'
-                        }
-                    >
-                        <i
-                            className={`far ${
-                                item.completed ? 'fa-check-square' : 'fa-square'
-                            }`}
-                        />
-                    </Button>
                 </Col>
                 <Col xs={10} className="name">
                     {item.name}
                 </Col>
                 <Col xs={1} className="text-center remove">
-                    <Button
-                        size="sm"
-                        variant="link"
-                        onClick={removeItem}
-                        aria-label="Remove Item"
-                    >
-                        <i className="fa fa-trash text-danger" />
-                    </Button>
                 </Col>
             </Row>
         </Container>
